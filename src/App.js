076cbaa1efa096e4339f2c0b9cb286ae309cc36e
Tika Yesi Kristiani <yesikristianitika@gmail.com>
2023-06-30 17:30:41 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Hello, Trainer. This is my simple docker image
         </p>
         <a
           className="App-link"
